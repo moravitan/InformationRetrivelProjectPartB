b5ca0130ecd62a10c7bc92a9e2d218b7f155febd
Inbaroth <inbaroth1993@gmail.com>
2018-12-19 00:38:29 +0200

    searcher
    (cherry picked from commit bac22974ef38f7145d6fc91e264bd66327eb1949)

--- src/main/java/Model/Searcher.java
+++ src/main/java/Model/Searcher.java
@@ -1,18 +1,36 @@
 package Model;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.TreeMap;
+import java.util.Vector;
 
 public class Searcher {
 
+    Ranker ranker;
+    Parse parse;
+    HashMap<String,Integer> query;
+    HashSet<String> cities;
+    static TreeMap<Integer, Vector<String>> result;
 
     /**
+     *
      * @param query
      * @param cities
      */
-    public List<String> read(String query, HashSet<String> cities){
-        return null;
+    public TreeMap<Integer, Vector<String>> processQuery(String query, HashSet<String> cities){
+        this.cities=cities;
+       result = new TreeMap<Integer, Vector<String>>();
+       this.query = new HashMap<>();
+        parse.parsing("111",query,false);
+        this.query = parse.getTermsMapPerDocument();
+       // ranker.rank(query, cities, "111");
+        return result;
+
     }
 
     /**
@@ -20,7 +38,34 @@ public class Searcher {
      * @param file
      * @param cities
      */
-    public List<String> read(File file, HashSet<String> cities){
-        return null;
+    public TreeMap<Integer, Vector<String>> processQuery(File file, HashSet<String> cities){
+        this.cities=cities;
+        result = new TreeMap<Integer, Vector<String>>();
+        query = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String st;
+            String id="";
+            String queryContent="";
+            while ((st = br.readLine()) != null ){
+                if (st.contains("<num> Number:")) {
+                    id = st.substring(14, 17);
+                }
+                if (st.contains("<title> ")){
+                    queryContent = st.substring(8);
+                }
+                if (!id.equals("") && !queryContent.equals("")) {
+                    parse.parsing(id, queryContent, false);
+                    query = parse.getTermsMapPerDocument();
+                    // ranker.rank(query, cities, id);
+                }
+
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        return result;
     }
+
+
+
 }
--- src/main/java/Model/testInbar.java
+++ src/main/java/Model/testInbar.java
@@ -1,8 +1,26 @@
 package Model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Vector;
+
 public class testInbar {
 
     public static void main(String[] args) {
-
+        // System.out.println("something");
+        // System.out.println("git is shit");
+        String str = "<num> Number: 352 ";
+        String str2 = "<num> Number: 367";
+        // System.out.println(str2.substring(14,17));
     }
+
+
+
+
+
 }
